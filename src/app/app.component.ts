2b0868b9e1c29f96b5217f102a6f2a2bb48e59b6
phenom_ayush_dixit <ayush.dixit@phenompeople.com>
2022-10-22 23:01:19 +0530

    feat: added feedback component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @Input('noOfStars') public noOfStars: number = 5; 
+  @Output('feedback') feedback = new EventEmitter;
+
   title = 'my-first-microfrontend-app';
+  noSelectedStars: number = 0;
+
+  updateFeedbackResponse(index: number) {
+      index++;
+      this.noSelectedStars = index;
+  }
+
+  submitFeedback() {
+    this.feedback.emit({
+      noOfStars: this.noOfStars
+    });
+  }
+
 }
