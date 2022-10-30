2818790b28af26a318592cdd7f35bc3ce5fd0af1
phenom_ayush_dixit <ayush.dixit@phenompeople.com>
2022-10-30 21:51:13 +0530

    added support for microfrontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { ApplicationRef, Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { createCustomElement } from '@angular/elements';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -15,4 +15,15 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    const mfe = createCustomElement(AppComponent, { injector });
+    customElements.define('feedback-app', mfe);
+  }
+
+  public ngDoBootstrap(appRef: ApplicationRef): void {
+    if (document.querySelector('feedback-app')) {
+      appRef.bootstrap(AppComponent);
+    }
+  }
+}
